466476c44d400c1302a9a546e4b0970fbb7ba8f6
John Mohlenkamp <john@mohlenkamp.com>
2021-09-11 12:18:57 -0500

    figured out issue with auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const { User, Book } = require('../models');
 
 const resolvers = {
 
@@ -5,6 +8,55 @@ const resolvers = {
         helloWorld: () => {
             return `Hello world`;
         }
+    },
+
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('Book')
+                          
+              return userData;
+            }
+          
+            throw new AuthenticationError('Not logged in');
+          }
+      },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+          
+            return { token, user };
+          },
+          login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const token = signToken(user);
+            return { token, user };
+          },
+          saveBook: async (parent, args) => {
+            const user = await User.find({ saveBook });
+
+            return
+          },
+          // removeBook: async () => {
+
+
+          //   return
+          // }
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,58 @@
 const { gql } = require('apollo-server-express');
 
+
+
 const typeDefs = gql`
+
+  
+    
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    input SaveBookInput{
+        authors: [String]
+        description: String
+        title: String
+        bookId: ID
+        image: String
+        link: String    
+    }
 
     type Query {
         helloWorld: String
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String): User
+    }
+
+
+    type Mutation {
+        login(email: String, password: String): Auth
+        addUser(username: String!, email: String!, password: String!):Auth
+        saveBook(bookData: SaveBookInput!):User
+        # removeBook(bookId: ID!):User
     }
 
 `;
